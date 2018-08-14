1e471b6ffc64647559bf89ef6c4f1c318b4a0481
Tetsuro Takemoto <t@ineeza.com>
2018-08-14 19:54:24 +0900

    wip: Dashbard style

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -16,6 +16,8 @@ import {
   Card,
   Profile,
   Container,
+  Table,
+  Form,
 } from "tabler-react"
 
 class MainPage extends React.Component {
@@ -26,7 +28,7 @@ class MainPage extends React.Component {
     return (
       <div>
         <Site>
-          <Site.Header><div style={titleStyle}>EconoMe</div></Site.Header>
+          <Site.Header><div style={titleStyle}>EconoMe Cloud Wallet</div></Site.Header>
           <Site.Nav
             items={
               <React.Fragment>
@@ -35,20 +37,112 @@ class MainPage extends React.Component {
                 <Nav.Item value="Register" icon="user-plus" to='/register'></Nav.Item>
               </React.Fragment>
             }
-          />
-          <Container>
-            <Page>
-              <Page.Header title="Dashboard"/>
-              <Card
-                title="Wallet Info"
-                body={`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
-    deleniti fugit incidunt, iste, itaque minima neque pariatur
-    perferendis sed suscipit velit vitae voluptatem. A consequuntur,
-    deserunt eaque error nulla temporibus!`}
-              />
-            </Page>
-          </Container>
-          <Site.Footer copyright='Ineeza, Inc.'></Site.Footer>
+      />
+      <Container>
+        <Page>
+          <Page.Header title="Dashboard"/>
+          <Card>
+            <Card.Header>
+              <Card.Title>My wallet info</Card.Title>
+              <Card.Options>
+              </Card.Options>
+            </Card.Header>
+            <Card.Body>
+              <Form.Group label="Network">
+                <Form.Select>
+                  <option>Rinkeby</option>
+                  <option>Ropsten</option>
+                  <option>Kovan</option>
+                  <option>Mainnet</option>
+                </Form.Select>
+              </Form.Group>
+              <Form.Group label="Ethereum Address">
+                <Form.Input
+                  name="eth-address"
+                  readOnly
+                  value="0xcb2c508ad5247df2ef60195fff8ae990adc16cbd"
+                />
+              </Form.Group>
+              <Form.Group label="ETH Balance">
+                <Form.Input
+                  name="eth-balance"
+                  readOnly
+                  value="0.999718636 ETH"
+                />
+              </Form.Group>
+              <Table hasOutline='true'>
+                <Table.Header>
+                  <Table.ColHeader>ERC20 Token Name</Table.ColHeader>
+                  <Table.ColHeader>Balance</Table.ColHeader>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Col>Tronix (TRX)</Table.Col>
+                    <Table.Col>1000</Table.Col>
+                    <Table.Col alignContent='right'>
+                      <Button.List>
+                        <Button color='primary'>Send</Button>
+                        <Button color='danger'>Remove</Button>
+                      </Button.List>
+                    </Table.Col>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Col>BNB (BNB)</Table.Col>
+                    <Table.Col>1000</Table.Col>
+                    <Table.Col alignContent='right'>
+                      <Button.List>
+                        <Button color='primary'>Send</Button>
+                        <Button color='danger'>Remove</Button>
+                      </Button.List>
+                    </Table.Col>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Col>OmiseGO (OMG)</Table.Col>
+                    <Table.Col>1000</Table.Col>
+                    <Table.Col alignContent='right'>
+                      <Button.List>
+                        <Button color='primary'>Send</Button>
+                        <Button color='danger'>Remove</Button>
+                      </Button.List>
+                    </Table.Col>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Col>VeChain (VEN)</Table.Col>
+                    <Table.Col>1000</Table.Col>
+                    <Table.Col alignContent='right'>
+                      <Button.List>
+                        <Button color='primary'>Send</Button>
+                        <Button color='danger'>Remove</Button>
+                      </Button.List>
+                    </Table.Col>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Col>ZRX (ZRX)</Table.Col>
+                    <Table.Col>1000</Table.Col>
+                    <Table.Col alignContent='right'>
+                      <Button.List>
+                        <Button color='primary'>Send</Button>
+                        <Button color='danger'>Remove</Button>
+                      </Button.List>
+                    </Table.Col>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Col>Maker (MKR)</Table.Col>
+                    <Table.Col>1000</Table.Col>
+                    <Table.Col alignContent='right'>
+                      <Button.List>
+                        <Button color='primary'>Send</Button>
+                        <Button color='danger'>Remove</Button>
+                      </Button.List>
+                    </Table.Col>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Card.Body>
+          </Card>
+        </Page>
+      </Container>
+      <Site.Footer copyright='Copyright © 2018 Ineeza, Inc.'></Site.Footer>
         </Site>
       </div>
     )
