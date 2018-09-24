bc46b1caf72068763ff175404d7e8ec4e1b51f00
Tetsuro Takemoto <t@ineeza.com>
2018-09-24 14:01:44 +0900

    style: Update Login, Logout, Signup

--- frontend/pages/logout.jsx
+++ frontend/pages/logout.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Layout from '../components/baselayout/'
 import cookie from 'js-cookie'
+import { Alert, Card } from 'tabler-react'
 
 cookie.remove('X-ECW-ACCESS-TOKEN')
 
 const Logout = () => (
   <Layout>
-    <h3>You have successfully logged out!</h3>
+    <Card title='Logout'>
+      <Card.Body>
+        <Alert type='warning'>You have successfully logged out!</Alert>
+      </Card.Body>
+    </Card>
   </Layout>
 )
 
