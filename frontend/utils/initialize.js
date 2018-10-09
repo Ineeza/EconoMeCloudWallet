fe2056717ccb5f87c789021bb08efe1ff96ff533
Tetsuro Takemoto <t@ineeza.com>
2018-10-09 16:49:16 +0900

    refactor: Remove prints

--- frontend/utils/initialize.js
+++ frontend/utils/initialize.js
@@ -6,12 +6,7 @@ export default function (ctx) {
   if (ctx.isServer) {
     if (ctx.req.headers.cookie) {
       // FIXME getCookie doesn't work
-      console.log('==== Initialize =====')
-      console.log('Cookies: ' + ctx.req.headers.cookie)
-      console.log('isServer: ' + ctx.isServer)
-      console.log('ctx.req: ' + ctx.req)
       const cookieStr = ctx.req.headers.cookie.replace('X-ECW-ACCESS-TOKEN=', '')
-      console.log('getCookie: ' + cookieStr)
       ctx.store.dispatch(actions.reauthenticate(cookieStr))
     }
   } else {
