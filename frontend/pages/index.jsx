9c67d86278df71f8d807bbbc2ff954a0e475badf
Tetsuro Takemoto <t@ineeza.com>
2018-11-08 17:33:37 +0900

    fix: Remove react-cookie

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -4,7 +4,6 @@ import initialize from '../utils/initialize'
 import BaseLayout from '../components/BaseLayout'
 import titleImg from '../static/wallet.png'
 import { Card } from 'tabler-react'
-import { CookiesProvider } from 'react-cookie'
 
 const titleStyle = {
   textAlign: 'center'
@@ -24,17 +23,15 @@ const imgStyle = {
 }
 
 const Index = () => (
-  <CookiesProvider>
-    <BaseLayout>
-      <Card>
-        <Card.Body>
-          <h1 style={titleStyle}>Create a wallet in seconds, using EconoMe API</h1>
-          <p style={subTitleStyle}>Cryptocurrencies and Collectibles</p>
-          <img style={imgStyle} src={titleImg} />
-        </Card.Body>
-      </Card>
-    </BaseLayout>
-  </CookiesProvider>
+  <BaseLayout>
+    <Card>
+      <Card.Body>
+        <h1 style={titleStyle}>Create a wallet in seconds, using EconoMe API</h1>
+        <p style={subTitleStyle}>Cryptocurrencies and Collectibles</p>
+        <img style={imgStyle} src={titleImg} />
+      </Card.Body>
+    </Card>
+  </BaseLayout>
 )
 
 Index.getInitialProps = (ctx) => {
