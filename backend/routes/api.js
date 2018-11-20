58b443e46183f98862651fc7fb660623a1c21be3
Tetsuro Takemoto <t@ineeza.com>
2018-11-20 18:22:20 +0900

    feat: API for Sending ETH

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -144,5 +144,65 @@ module.exports = (app, server) => {
     })
   })
 
+  router.post('/eth', [
+    check('password').exists(),
+    check('recipientAddress').exists(),
+    check('amount').exists()
+  ], (req, res, next) => {
+    // Error handling
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+    Account.findOne({ where: { email: req.user.email } }).then(account => {
+      console.log('==== POST /eth ====')
+      console.log(req.body)
+      const userId = account.id
+      const password = req.body.password
+      const recipientAddress = req.body.recipientAddress
+      const amount = req.body.amount
+      console.log('userId: ' + userId)
+      console.log('recipientAddress: ' + recipientAddress)
+      console.log('amount: ' + amount)
+
+      Keystore.findOne({ where: { account_id: userId } }).then(keystore => {
+        const myKeyObject = JSON.parse(keystore.content)
+        let myWalletAddress = '0x' + myKeyObject.address
+        console.log('My Wallet Address: ' + myWalletAddress)
+        // Nonce
+        const web3 = new Web3(new Web3.providers.HttpProvider(networks.rinkeby))
+        web3.eth.getTransactionCount(myWalletAddress).then((nonce) => {
+          console.log('Nonce: ' + nonce)
+          // Private Key
+          const privateKey = keythereum.recover(password, myKeyObject)
+          // Signed Transaction
+          let toAddress = recipientAddress
+          const decimalAmount = web3.utils.toWei(amount, 'ether')
+          console.log('===== Creating Transaction =====')
+          console.log('  amount(Wei): ' + decimalAmount)
+          console.log('  amount(Hex): ' + web3.utils.toHex(decimalAmount))
+          console.log('  toAddress: ' + toAddress)
+          console.log('  gasPrice: ' + web3.utils.toHex(4.8 * 1e9))
+          console.log('  gasLimit: ' + web3.utils.toHex(210000))
+          console.log('  nounce: ' + web3.utils.toHex(nonce))
+          // DEBUG
+          const rawTransaction = {
+            'from': myWalletAddress,
+            'gasPrice': web3.utils.toHex(2 * 1e9),
+            'gasLimit': web3.utils.toHex(210000),
+            'to': recipientAddress,
+            'value': web3.utils.toHex(decimalAmount),
+            'data': '0x0',
+            'nonce': web3.utils.toHex(nonce)
+          }
+          const transaction = new Tx(rawTransaction)
+          transaction.sign(privateKey)
+          web3.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
+            .on('transactionHash', (hash) => res.json({ 'transactionHash': hash }))
+        })
+      })
+    })
+  })
+
   return router
 }
