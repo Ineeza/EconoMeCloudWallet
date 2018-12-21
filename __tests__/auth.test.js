ef06f3344a0d34612f92d2077985ce47b1195637
Tetsuro Takemoto <t@ineeza.com>
2018-12-21 20:14:51 +0900

    test: Delete all records before all

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -1,7 +1,33 @@
 const request = require('supertest')
 const { app } = require('../backend/app')
+const { Account, Keystore } = require('../backend/model')
+
+beforeAll(() => {
+  // Delete all records
+  Keystore.destroy({ where: {}, truncate: true })
+  Account.destroy({ where: {}, truncate: true })
+})
+
+afterAll(() => {
+  // Delete all records
+  Keystore.destroy({ where: {}, truncate: true })
+  Account.destroy({ where: {}, truncate: true })
+})
 
 test('[POST /auth/signup] signup successfully', (done) => {
+  request(app)
+    .post('/auth/signup')
+    .send({
+      email: 'test@example.com',
+      password: '123'
+    })
+    .then((response) => {
+      expect(response.statusCode).toBe(200)
+      done()
+    })
+})
+
+test('[POST /auth/signup] reject if username is already used', (done) => {
   request(app)
     .post('/auth/signup')
     .send({
