0429070bb9c1a98a90d2037b381f56f0c02e28bf
Tetsuro Takemoto <t@ineeza.com>
2019-05-25 14:20:33 +0900

    wip: Generate private key on the client side

--- frontend/pages/mnemonic.jsx
+++ frontend/pages/mnemonic.jsx
@@ -2,6 +2,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import axiosBase from 'axios'
+import { bindActionCreators } from 'redux'
+import actions from '../actions'
 import { apiHost } from '../../backend/config'
 import initialize from '../utils/initialize'
 import Layout from '../components/BaseLayout'
@@ -9,40 +11,59 @@ import { JWT_KEY } from '../constants/keys'
 // $FlowFixMe
 import { Card } from 'tabler-react'
 
-const Mnemonic = ({ user, mnemonic }: Object) => (
-  <Layout title='Mnemonic'>
-    <Card title='Mnemonic Code'>
-      <Card.Body>
-        <strong>{mnemonic}</strong>
-      </Card.Body>
-    </Card>
-  </Layout>
-)
+const mapStateToProps = (state) => {
+  return {}
+}
 
-Mnemonic.getInitialProps = async (ctx) => {
-  initialize(ctx)
-  const token = ctx.store.getState().authentication.token
-  const axios = axiosBase.create({
-    baseURL: apiHost,
-    headers: {
-      [JWT_KEY]: token
+const mapDispachToProps = (dispatch: Dispatch<*>) => {
+  return bindActionCreators(actions, dispatch)
+}
+
+class MnemonicPage extends React.Component<Props, State> {
+  static async getInitialProps (ctx) {
+    initialize(ctx)
+    const token = ctx.store.getState().authentication.token
+    const axios = axiosBase.create({
+      baseURL: apiHost,
+      headers: {
+        [JWT_KEY]: token
+      }
+    })
+
+    // TODO Create Mnemonic
+    // FIXME Use globally imported bip39 module
+    var bip39 = require('bip39')
+    const mnemonic = bip39.generateMnemonic()
+
+    if (token) {
+      const response = await axios.get(`/api/profile`)
+      const user = response.data.account.email
+      return {
+        user,
+        mnemonic
+      }
     }
-  })
+  }
 
-  // TODO Create Mnemonic
-  // FIXME Use globally imported bip39 module
-  const bip39 = require('bip39')
-  const mnemonic = bip39.generateMnemonic()
-  console.log(mnemonic)
-
-  if (token) {
-    const response = await axios.get(`/api/profile`)
-    const user = response.data.account.email
-    return {
-      user,
-      mnemonic
+  constructor (props) {
+    super(props)
+    this.state = {
+      user: props.user,
+      mnemonic: props.mnemonic
     }
+  }
+
+  render () {
+    return (
+      <Layout title='Mnemonic'>
+        <Card title='Mnemonic Code'>
+          <Card.Body>
+            <strong>{this.state.mnemonic}</strong>
+          </Card.Body>
+        </Card>
+      </Layout>
+    )
   }
 }
 
-export default connect()(Mnemonic)
+export default connect(mapStateToProps, mapDispachToProps)(MnemonicPage)
