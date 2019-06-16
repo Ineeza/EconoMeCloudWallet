f05113a7c125a8dda52fb99f58c5748783cf3322
n0ts <naoya.n@gmail.com>
2019-06-16 18:40:50 +0900

    Add logging for using ECW_JWT_SECRET

--- backend/middleware/jwtSecret.js
+++ backend/middleware/jwtSecret.js
@@ -1,4 +1,10 @@
+const logger = require('./logger')
+
 // TODO delete default top_secret
 const secretKey = process.env.ECW_JWT_SECRET || 'top_secret'
+
+if (secretKey == 'top_secret') {
+    logger.debug('ECW_JWT_SECRET is defined')
+}
 
 module.exports = secretKey
