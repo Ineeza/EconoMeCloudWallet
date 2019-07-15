b7ba4dfdae119e5a51433e37c98e25651d9523bf
Tetsuro Takemoto <t@ineeza.com>
2019-07-15 17:39:24 +0900

    feat: Set cookies to never expire

--- frontend/utils/cookie.js
+++ frontend/utils/cookie.js
@@ -3,7 +3,7 @@ import cookie from 'js-cookie'
 export const setCookie = (key, value) => {
   if (process.browser) {
     cookie.set(key, value, {
-      expires: 1,
+      expires: 36500,
       path: '/'
     })
   }
